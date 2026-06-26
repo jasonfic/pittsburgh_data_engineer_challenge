e80f7fbed3a0ea6e47507eb8fd1136879e8dcde0
jasonficorilli@gmail.com <jasonficorilli@gmail.com>
2026-06-26 12:35:23 +0000

    Final Comments, Table Name Correction

--- models/dim_skater_careers.sql
+++ models/dim_skater_careers.sql
@@ -7,7 +7,7 @@ WITH source_data AS (
         SUM(assists) AS career_assists, SUM(points) AS career_points,
         ROUND(SUM(toi_seconds) / 60.0, 1) AS career_toi_minutes,
         ROUND(SUM(points)/SUM(gp), 2) AS career_ppg
-    FROM `trim-icon-437420-r5.pens_interview_skater_seasons.skater_seasons`
+    FROM `skater_seasons`
     GROUP BY nhl_player_id, player_name, position_group
     HAVING SUM(toi_seconds) > 10000
 
